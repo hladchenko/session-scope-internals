dbe897e0e904a02bfc9c92241d9776c5589be78d
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-27 21:36:13 +0300

    init

---
+++ src/main/java/com/hladchenko/sessionscopeinternals/SessionScopeInternalsApplication.java
@@ -0,0 +1,13 @@
+package com.hladchenko.sessionscopeinternals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionScopeInternalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionScopeInternalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hladchenko/sessionscopeinternals/SessionScopeInternalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hladchenko.sessionscopeinternals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionScopeInternalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
